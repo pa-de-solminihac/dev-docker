b514830f72d1685b81138fded0f5a7cb3adb4338
Pierre-Alexis de Solminihac <pa@quai13.com>
2015-08-03 17:57:52 +0200

    fix: persistant mysql (init with mysql_install_db if necessary) new: phpmyadmin (with /phpmyadmin alias)

--- site/run.sh
+++ site/run.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
+
+# start apache
 source /etc/apache2/envvars
 exec apache2 -D FOREGROUND &
+
+# start mysql avec initialisation de la BD si necessaire
+if [ ! -d /var/lib/mysql/mysql ]; then
+    mysql_install_db
+fi
 exec mysqld_safe
 
