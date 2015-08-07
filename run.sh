10ff6f8b974ab5d95f591309c20cb46d9fa26eeb
Pierre-Alexis de Solminihac <pa@quai13.com>
2015-08-07 17:21:44 +0200

    new: sitesync with multiple sites new: dockersite base folders

--- run.sh
+++ run.sh
@@ -10,7 +10,13 @@ fi
 
 DEVDOCKER_ID="$(docker ps | grep dockersite | head -n 1 | awk '{print $1}')"
 if [ "$DEVDOCKER_ID" == "" ]; then
-    DEVDOCKER_ID="$(docker run -d -i -p 80:80 -v "$DOCKERSITE_ROOT/www:/var/www/html" -v "$DOCKERSITE_ROOT/dockersite/database:/var/lib/mysql" -v "$DOCKERSITE_ROOT/dockersite/vhosts:/etc/apache2/vhosts" -v "$DOCKERSITE_ROOT/dockersite/log:/var/log/dockersite" dockersite)"
+    DEVDOCKER_ID="$(docker run -d -i -p 80:80 \
+        -v "$DOCKERSITE_ROOT/www:/var/www/html" \
+        -v "$DOCKERSITE_ROOT/database:/var/lib/mysql" \
+        -v "$DOCKERSITE_ROOT/vhosts:/etc/apache2/vhosts" \
+        -v "$DOCKERSITE_ROOT/log:/var/log/dockersite" \
+        -v "$DOCKERSITE_ROOT/conf-sitesync:/sitesync/etc" \
+        dockersite)"
     echo "Attaching to freshly started container $DEVDOCKER_ID"
 else
     echo "Attaching to already running container $DEVDOCKER_ID"
