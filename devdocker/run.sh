73f9712e14a805cd22fdf8e62297af9f7a608881
Pierre-Alexis de Solminihac <pa@quai13.com>
2015-08-10 11:34:17 +0200

    fix: config phpmyadmin

--- devdocker/run.sh
+++ devdocker/run.sh
@@ -7,6 +7,8 @@ exec apache2 -D FOREGROUND &
 # start mysql avec initialisation de la BD si necessaire
 if [ ! -d /var/lib/mysql/mysql ]; then
     mysql_install_db
+    # install phpmyadmin database
+    zcat /usr/share/doc/phpmyadmin/examples/create_tables.sql.gz | mysql
 fi
 exec mysqld_safe
 
