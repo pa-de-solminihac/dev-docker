e295dce26db0948a0642d4fcce728587a6c7bc73
Pierre-Alexis de Solminihac <pa@quai13.com>
2015-08-12 10:33:08 +0200

    fix: "file does not exist" warning

--- boot2docker_start.sh
+++ boot2docker_start.sh
@@ -2,5 +2,5 @@
 boot2docker start
 eval "$(boot2docker shellinit 2> /dev/null)"
 # allow quai2.quai13.com if not already allowed
-boot2docker ssh "grep -q \"quai2.quai13.com:5000\" /var/lib/boot2docker/profile || echo $'EXTRA_ARGS=\"--insecure-registry quai2.quai13.com:5000\"' | sudo tee -a /var/lib/boot2docker/profile && sudo /etc/init.d/docker restart"
+boot2docker ssh "grep -sq \"quai2.quai13.com:5000\" /var/lib/boot2docker/profile || echo $'EXTRA_ARGS=\"--insecure-registry quai2.quai13.com:5000\"' | sudo tee -a /var/lib/boot2docker/profile && sudo /etc/init.d/docker restart"
 boot2docker ip
