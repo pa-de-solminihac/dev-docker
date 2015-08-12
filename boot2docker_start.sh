21f93d1db4e89f9e5ff3f38fa444053f9c8881bb
Pierre-Alexis de Solminihac <pa@quai13.com>
2015-08-12 10:20:17 +0200

    new: add local registry to boot2docker

--- boot2docker_start.sh
+++ boot2docker_start.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 boot2docker start
 eval "$(boot2docker shellinit 2> /dev/null)"
+# allow quai2.quai13.com if not already allowed
+boot2docker ssh "grep -q \"quai2.quai13.com:5000\" /var/lib/boot2docker/profile || echo $'EXTRA_ARGS=\"--insecure-registry quai2.quai13.com:5000\"' | sudo tee -a /var/lib/boot2docker/profile && sudo /etc/init.d/docker restart"
 boot2docker ip
