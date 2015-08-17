be2d968ca592e67f60dfa95f2258d472a2178512
Pierre-Alexis de Solminihac <pa@quai13.com>
2015-08-17 15:06:08 +0200

    fix: startup scripts verbosity

--- boot2docker_start.sh
+++ boot2docker_start.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
+echo "1/3) Starting boot2docker VM"
 boot2docker start
+
+echo "2/3) Setting environment variables"
 eval "$(boot2docker shellinit 2> /dev/null)"
-# allow quai2.quai13.com if not already allowed
+
+echo "3/3) Allowing local repository if necessary"
 boot2docker ssh "grep -sq \"quai2.quai13.com:5000\" /var/lib/boot2docker/profile || echo $'EXTRA_ARGS=\"--insecure-registry quai2.quai13.com:5000\"' | sudo tee -a /var/lib/boot2docker/profile && sudo /etc/init.d/docker restart"
-boot2docker ip
--- boot2docker_stop.sh
+++ boot2docker_stop.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
+echo "Stopping boot2docker VM"
 boot2docker stop
