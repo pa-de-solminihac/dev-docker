0428f7039b4e2cd8fc16f3a565e8df5fe41566b4
Pierre-Alexis de Solminihac <pa@quai13.com>
2015-08-27 00:19:46 +0200

    fix(scripts): silent VM error messages when removing port forwarding

--- vm-stop.sh
+++ vm-stop.sh
@@ -12,11 +12,11 @@ then
     fi
     echo "Stopping Docker VM (and all containers)"
     # delete port forwarding rules
-    VBoxManage controlvm "$DEVDOCKER_VM" natpf1 delete "tcp-port-80"
-    VBoxManage controlvm "$DEVDOCKER_VM" natpf1 delete "udp-port-80"
-    VBoxManage controlvm "$DEVDOCKER_VM" natpf1 delete "tcp-port-443"
-    VBoxManage controlvm "$DEVDOCKER_VM" natpf1 delete "udp-port-443"
-    VBoxManage controlvm "$DEVDOCKER_VM" natpf1 delete "tcp-port-3306"
-    VBoxManage controlvm "$DEVDOCKER_VM" natpf1 delete "udp-port-3306"
+    VBoxManage controlvm "$DEVDOCKER_VM" natpf1 delete "tcp-port-80" > /dev/null 2>&1
+    VBoxManage controlvm "$DEVDOCKER_VM" natpf1 delete "udp-port-80" > /dev/null 2>&1
+    VBoxManage controlvm "$DEVDOCKER_VM" natpf1 delete "tcp-port-443" > /dev/null 2>&1
+    VBoxManage controlvm "$DEVDOCKER_VM" natpf1 delete "udp-port-443" > /dev/null 2>&1
+    VBoxManage controlvm "$DEVDOCKER_VM" natpf1 delete "tcp-port-3306" > /dev/null 2>&1
+    VBoxManage controlvm "$DEVDOCKER_VM" natpf1 delete "udp-port-3306" > /dev/null 2>&1
     $DOCKERMACHINE --native-ssh stop $DEVDOCKER_VM
 fi
