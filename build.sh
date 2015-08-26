79bd04e2ab738259f2e9e3a86b2b3c9f3119451e
Pierre-Alexis de Solminihac <pa@quai13.com>
2015-08-27 00:06:23 +0200

    feat(scripts): pass build.sh options to docker build

--- build.sh
+++ build.sh
@@ -15,7 +15,7 @@ then
     #env | grep DOCKER
 fi
 
-docker build --no-cache -f devdocker/Dockerfile -t quai2.quai13.com:5000/devdocker:latest devdocker
+docker build $@ -f devdocker/Dockerfile -t quai2.quai13.com:5000/devdocker:latest devdocker
 cat <<EOL
 Now you can tag and push the image:
     docker push quai2.quai13.com:5000/devdocker
