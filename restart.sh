5180f99a1c1b8d8107801ff31c124b4a7df29d7a
Pierre-Alexis de Solminihac <pa@quai13.com>
2016-03-15 14:03:13 +0100

    chore(scripts): cleanup

--- restart.sh
+++ restart.sh
@@ -1,15 +1,4 @@
 #!/usr/bin/env bash
-
-BASE_PATH="$(dirname "$0")"
-source $BASE_PATH/inc/init
-
-# run docker-machine VM if necessary
-if [ -x "$DOCKERMACHINE_PATH" ]; then
-    # ask for root password as early as possible
-    if [ -x "$(which sudo 2> /dev/null)" ]; then
-        sudo echo -n # ask for root password only once
-    fi
-fi
 ./stop.sh
 echo
 ./start.sh
