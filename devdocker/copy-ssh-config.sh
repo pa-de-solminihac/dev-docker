03ec8726ef35f45bf07160b6e09b0ffb2628301f
Pierre-Alexis de Solminihac <pa@quai13.com>
2016-03-18 23:58:48 +0100

    fix(ssh): do not importe .ssh/config file inside container in order to avoid side effects (caused by ControlMaster or ControlPath) fix(dockerfile): better docker cache usage feat(bash): enable bash configuration for root user too

--- devdocker/copy-ssh-config.sh
+++ devdocker/copy-ssh-config.sh
@@ -3,3 +3,5 @@ cp -f /home/devdocker/.ssh-readonly/* /root/.ssh/ 2>/dev/null
 cp -f /home/devdocker/.ssh-readonly/* /home/devdocker/.ssh/ 2>/dev/null
 chown -R root: /root/.ssh
 chown -R devdocker: /home/devdocker/.ssh
+rm -f /root/.ssh/config
+rm -f /home/devdocker/.ssh/config
--- start.sh
+++ start.sh
@@ -58,7 +58,7 @@ if [ "$DEVDOCKER_ID" == "" ]; then
     # save container hosts file before we complete it with every login
     docker exec "$DEVDOCKER_ID" sh -c "cp /etc/hosts /etc/hosts.ori"
     # wait enough time for usermod to finish cleanly in run.sh
-    sleep 5
+    sleep 3
 else
     echo -ne "\033$TERM_COLOR_YELLOW"
     echo "# Attaching to already running container: "
