76ca94af43350b487a4532fa63c9a275fe3ce9a8
Pierre-Alexis de Solminihac <pa@quai13.com>
2016-06-17 14:57:26 +0200

    refacto(usermode): remove references to usermode since it's merged in master now

--- build.sh
+++ build.sh
@@ -16,7 +16,7 @@ fi
 BUILD_OK=0
 # bust cache if git repository updated
 export CACHEBUST="`git ls-remote https://github.com/pa-de-solminihac/configuration.git | grep refs/heads/master | cut -f 1`"
-BUILD_CMD="docker build $@ --build-arg CACHEBUST=$CACHEBUST -f devdocker_usermode/Dockerfile -t $DEVDOCKER_IMAGE:latest devdocker_usermode "
+BUILD_CMD="docker build $@ --build-arg CACHEBUST=$CACHEBUST -f devdocker/Dockerfile -t $DEVDOCKER_IMAGE:latest devdocker"
 $BUILD_CMD && BUILD_OK=1
 if [ "$BUILD_OK" == "1" ]; then
     if [ -x "$DOCKERMACHINE_PATH" ]; then
