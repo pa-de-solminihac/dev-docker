967c3460d903bae93582b0d39d238f416932f25a
pa-de-solminihac <pa@quai13.com>
2018-08-31 17:37:07 +0200

    Revert "improv(script): better status infos, with containers logs"
    This reverts commit 94fe60fb6bf673ec6b25f50c3ab168a93f7e1fdd.

--- status.sh
+++ status.sh
@@ -19,18 +19,10 @@ fi
 echo -ne "\033$TERM_COLOR_GREEN"
 echo -n "# Devdocker containers running: "
 echo -e "\033$TERM_COLOR_NORMAL"
-echo "$DEVDOCKER_CONTAINERS" | awk '{print $1"\t"$2}'
+docker ps | head -n 1
+echo "$DEVDOCKER_CONTAINERS"
 
 echo
-echo -ne "\033$TERM_COLOR_YELLOW"
-echo -n "# Devdocker logs: "
-echo -e "\033$TERM_COLOR_NORMAL"
-DEVDOCKER_CONTAINERS_IDS="$(echo "$DEVDOCKER_CONTAINERS" | awk '{print $1}')"
-for ID in "$DEVDOCKER_CONTAINERS_IDS"; do
-    echo "docker logs -f $ID"
-    docker logs $ID
-done
-
 if [ -x "$DOCKERMACHINE" ]; then
     source $BASE_PATH/inc/vm-eval
     PORT_FW_PID="$(ps auwx | (grep "$SSH_PORT_FW_CMD" | grep -v 'grep' | grep -v 'sudo' || true) | awk '{print $2}')";
